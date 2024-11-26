878515095faebb85482a2fe95c807385843e5e89
emonShimoul <emonshimoul@gmail.com>
2024-11-26 17:09:03 +0600

    completed testimonials section

--- index.php
+++ index.php
@@ -2,5 +2,6 @@
 
 <?php get_template_part("banner"); ?>
 <?php get_template_part("services"); ?>
+<?php get_template_part("testimonials"); ?>
 </body>
 </html>
---
+++ testimonials.php
@@ -0,0 +1,33 @@
+<div class="testimonials bg-gray-100 px-20 pb-28">
+    <p class="uppercase text-primaryColor font-semibold mb-1"><i class="fa-brands fa-think-peaks"></i> testimonials</p>
+    <h4 class="text-2xl font-daysOneRegular uppercase mb-1">What our clients says</h4>
+
+    <div class="flex justify-between gap-8 mt-5">
+        <div class="bg-white p-8">
+            <i class="fa-solid fa-quote-left text-4xl text-primaryColor"></i>
+            <p class="mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <div>
+                <h5 class="text-lg text-primaryColor font-daysOneRegular mt-4">Bradford B. Pounds</h5>
+                <p>Santa Cruz, CA</p>
+            </div>
+        </div>
+
+        <div class="bg-white p-8">
+            <i class="fa-solid fa-quote-left text-4xl text-primaryColor"></i>
+            <p class="mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <div>
+                <h5 class="text-lg text-primaryColor font-daysOneRegular mt-4">Bradford B. Pounds</h5>
+                <p>Santa Cruz, CA</p>
+            </div>
+        </div>
+
+        <div class="bg-white p-8">
+            <i class="fa-solid fa-quote-left text-4xl text-primaryColor"></i>
+            <p class="mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <div>
+                <h5 class="text-lg text-primaryColor font-daysOneRegular mt-4">Bradford B. Pounds</h5>
+                <p>Santa Cruz, CA</p>
+            </div>
+        </div>
+    </div>
+</div>
