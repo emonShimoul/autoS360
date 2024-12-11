0d8993b04f5f7de60694c843417bd2209930bd78
emonShimoul <emonshimoul@gmail.com>
2024-12-11 18:18:01 +0600

    introduced cars page instead of product

---
+++ page-cars.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Template Name: Cars
+*/
+?>
+
+<?php get_header(); ?>
+
+<?php if(! is_front_page()){ ?>
+<div class="">
+    <h1 class="mx-20 py-48 text-center text-4xl text-blue-500">This is Cars Page</h1>
+</div>
+<?php } ?>
+
+<?php get_footer(); ?>
