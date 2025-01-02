a291ef021425d1467c33a0031e8d5a63eaa6ab28
emonShimoul <emonshimoul@gmail.com>
2025-01-02 17:23:24 +0600

    changes lorem ipsum text and added som information of footer section

--- footer.php
+++ footer.php
@@ -1,16 +1,16 @@
 <div class="autos360-footer bg-[#2B4448] text-white px-20 py-10 flex justify-between gap-8">
     <div class="w-2/5">
         <h4 class="text-white font-daysOneRegular text-3xl"><span class="text-[#D42C2E]">AutoS</span>360</h4>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque repudiandae dolorem libero laborum deserunt, dicta debitis placeat vitae ab iusto nemo saepe natus quae vel ex rem quia esse illum qui ducimus aliquam odit fugit enim? Distinctio officia repellendus error quis maxime quisquam similique sed ipsa, iusto unde quaerat velit. </p>
+        <p>we understand the importance of finding the perfect car, whether it’s for personal or business use. Our inventory features a wide range of Japanese reconditioned cars, known for their superior performance, durability, and value. We also offer brand-new and used cars, ensuring there’s something for every preference. </p>
         <p class="text-sm text-gray-400 mt-3">Copyright © 2020 AutoS360. All Rights Reserved.</p>
     </div>
     <div class="w-2/5 flex flex-col">
         <div class="google-map mb-3">
             <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d177.6550799537237!2d90.4374811!3d23.7992287!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c7b452e30097%3A0x6abc60d6987da177!2sActive%20LPG%20Conversion%20Workshop!5e1!3m2!1sen!2sbd!4v1729846775198!5m2!1sen!2sbd" width="300" height="150" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
-        <p>Address: 100 Feet Road, Natun Bazar, Vatara, Dhaka</p>
-        <p>Phone: 01671-576080 , 01768-675770</p>
-        <p>Email: lpgactive@gmail.com</p>
+        <p>Address: 100 feet Road, Baridhara, dhaka.</p>
+        <p>Phone: +880 1628-164451, +880 1706-753955</p>
+        <p>Email: helpdiskbangla@gmail.com</p>
     </div>
     <div class="w-1/5 text-lg flex flex-col gap-4 items-end">
         <a href="#">About Us</a>
