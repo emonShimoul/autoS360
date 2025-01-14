fd09c693280dae0110f088d7940a27fd50d6e3e3
emonShimoul <emonshimoul@gmail.com>
2025-01-14 22:34:58 +0600

    remove unnecessary comments

--- slider.php
+++ slider.php
@@ -1,9 +1,5 @@
 <div class="slider-container rounded-2xl mb-8 mt-24">
   <div class="slider">
-    <!-- <div class="slide" style="background-color: #ff6b6b;">Slide 1</div> -->
-    <!-- <div class="slide" style="background-color: #6b8eff;">Slide 2</div>
-    <div class="slide" style="background-color: #6bff9e;">Slide 3</div> -->
-
     <div class="slide">
       <!--<img src="https://i.ibb.co.com/1b9n7C8/slider-1.jpg" alt="">-->
       <img src="https://i.ibb.co.com/nf6MC5N/web-slider.png" alt="">
