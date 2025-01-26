24f612039e436a99bf4b2da070faf232ca4c3fda
emonShimoul <emonshimoul@gmail.com>
2025-01-26 23:02:39 +0600

    added a team section

--- front-page.php
+++ front-page.php
@@ -5,5 +5,6 @@
 <?php get_template_part("services"); ?>
 <?php get_template_part("testimonials"); ?>
 <?php get_template_part("contact"); ?>
+<?php get_template_part("our-team"); ?>
 
 <?php get_footer(); ?>
