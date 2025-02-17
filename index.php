b8a45830b5a4a8d4d5d721e10b10731f64322a23
emonShimoul <emonshimoul@gmail.com>
2025-02-17 13:50:41 +0600

    remove unnecessary comments

--- index.php
+++ index.php
@@ -2,14 +2,13 @@
 get_header();
 
 if (is_home()) {
-    // Display blog posts
+    
     ?>
     
     
     
 <?php
 } else {
-    // Fallback for other pages
     echo '<p>This is the default index template.</p>';
 }
 
